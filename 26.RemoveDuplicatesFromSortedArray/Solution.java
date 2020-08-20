acce10994e4bc90f4faff6df2f5e67f3ec29bd27
Deep Panchal <deep302001@gmail.com>
2020-08-19 20:28:39 -0500

    Solve 26.RemoveDuplicatesFromSortedArray

---
+++ 26.RemoveDuplicatesFromSortedArray/Solution.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Solution {
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0)
+            return 0;
+        int len = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[len] != nums[i]) {
+                len++;
+                nums[len] = nums[i];
+            }
+        }
+
+        return len + 1;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 1, 1, 1, 2, 3, 4, 4, 4, 7 };
+        System.out.println("Initial Array: " + Arrays.toString(arr));
+        int finalArrLen = removeDuplicates(arr);
+        System.out.println("Filtered Array (Length: " + finalArrLen + "): " + Arrays.toString(arr));
+    }
+}
